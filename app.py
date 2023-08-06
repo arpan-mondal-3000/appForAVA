b8bd6e7a5ed752f75a7103ccee293fe761713a66
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2023-08-06 20:39:58 +0530

    Implemented flask sessions

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 @app.route('/')
 def index():
+    if not session.get("name"):
+        return redirect("/login")
+    return render_template("index.html", name=session.get("name"))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        return redirect("/")
     return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("/")
 
 
 if __name__ == "__main__":
