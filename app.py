d27cd73ae73c421fe95b1869cd4a9abc0406522b
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2023-08-28 12:16:46 +0530

    Added search functionality

--- app.py
+++ app.py
@@ -43,7 +43,29 @@ def logout():
 def birthday_finder():
     if not session.get("name"):
         return redirect("/login")
-    return render_template("birthdayfinder.html")
+    return render_template("birthdayfinder.html", text="Forgot his/her birthday? Not to worry, you are just one search away 😊")
+
+
+@app.route("/search")
+def search():
+    if not session.get("name"):
+        return redirect("/login")
+    try:
+        name = request.args.get("person_name")
+        connection = sqlite3.connect("userdata.db")
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT dob FROM userdata WHERE name LIKE '{name}%'")
+        rows = cursor.fetchall()
+        if rows != []:
+            dob = rows[0][0]
+            birthdate = dob[:5]
+            cursor.close()
+            connection.close()
+
+        text = f"{name}'s birthday is on {birthdate}"
+        return render_template("birthdayfinder.html", text=text, party_emoji="🥳", confetti="🎉")
+    except:
+        return render_template("birthdayfinder.html", error_text="Name not found in the database, try with a different name!")
 
 
 if __name__ == "__main__":
